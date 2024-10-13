d7a398b01b1df4e251c1f291242d8daf4a056739
eustachekamala <eustachejared25@gmail.com>
2024-10-13 22:20:13 +0300

    add the methods of create plants and GET plants

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask import Flask, jsonify, request, make_response # type: ignore
+from flask_migrate import Migrate # type: ignore
+from flask_restful import Api, Resource # type: ignore
 
 from models import db, Plant
 
@@ -17,11 +17,46 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
-
-class PlantByID(Resource):
-    pass
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
+    
+        response = make_response(
+            response_dict_list, 200
+        )
+    
+        return response
+    
+    def post(self):
+        new_plant = Plant(
+            name=request.form['name'],
+            image=request.form['image'],
+            price=request.form['price']
+        )
         
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        response_dict = new_plant.to_dict()
+        
+        response = make_response(
+            response_dict,
+            200,
+        )
+        
+        return response
+
+api.add_resource(Plants, '/plants')
+class PlantByID(Resource):
+    def get(self,id):
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+        
+        response = make_response(
+            response_dict,
+            200,
+        )
+        
+        return response
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from sqlalchemy_serializer import SerializerMixin # type: ignore
 
 db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String(200))
+    price = db.Column(db.Float)
